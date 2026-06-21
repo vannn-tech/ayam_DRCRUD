b5f685d1e0f17a1bd8b9de691e950100fd835493
vannn-tech <jovan20166@gmail.com>
2026-06-21 18:18:58 +0700

    resetData

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -137,6 +137,19 @@ namespace CRUDMahasiswaADO
             cmd.ExecuteNonQuery();
         }
 
+        public void resetData()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+            string deleteQuery = "DELETE FROM mahasiswa;";
+            SqlCommand cmdDelete = new SqlCommand(deleteQuery, conn);
+            cmdDelete.ExecuteNonQuery();
+            string insertQuery = @"INSERT INTO mahasiswa SELECT * FROM mahasiswa_backup;";
+            SqlCommand cmdInsert = new SqlCommand(insertQuery, conn);
+            cmdInsert.ExecuteNonQuery();
+        }
 
     }
 }
