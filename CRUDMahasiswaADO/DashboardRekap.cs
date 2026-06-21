1b2cbcb069370a0c0e1276b3dedc56f58f9bf662
vannn-tech <jovan20166@gmail.com>
2026-06-21 18:20:05 +0700

    logika class dal

--- CRUDMahasiswaADO/DashboardRekap.cs
+++ CRUDMahasiswaADO/DashboardRekap.cs
@@ -9,3 +9,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting; // Pastikan namespace ini ditambahkan untuk Chart
+
+namespace CRUDMahasiswaADO
+{
+    public partial class DashboardRekap : Form
+    {
+        // 8. Tambahkan Code berikut pada class Form Dashboard
+        DAL dbLogic = new DAL();
+        bool isInitializing = true;
+        DataTable dt;
+        int button = 0;
+
+        
+    }
+}
