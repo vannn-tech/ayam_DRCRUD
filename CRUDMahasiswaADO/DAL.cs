aee23c2aa79acf8828fad2a5ecd2ce03e9cf7f15
vannn-tech <jovan20166@gmail.com>
2026-06-21 18:17:37 +0700

    GetLoacalIPAddress

--- CRUDMahasiswaADO/DAL.cs
+++ CRUDMahasiswaADO/DAL.cs
@@ -25,5 +25,28 @@ namespace CRUDMahasiswaADO
         DataTable dtMahasiswa;
         DataTable dtProdi;
 
+        public static string GetLoacalIPAddress()
+        {
+            string localIP = string.Empty;
+            try
+            {
+                var host = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName());
+                foreach (var ip in host.AddressList)
+                {
+                    if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        localIP = ip.ToString();
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error getting local IP address: " + ex.Message);
+            }
+            return localIP;
+        }
+
+
     }
 }
